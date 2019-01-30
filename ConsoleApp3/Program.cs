e27d4bbfb6d9af120ec10bb0240fd5cce996924c
visualstudio <visualstudio@122-PC14>
2019-01-30 11:46:51 -0500

    first change

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,8 +8,18 @@ namespace ConsoleApp3
 {
     class Program
     {
-        static void Main(string[] args)
+        class Village
         {
+            // Node is an ADT
+            // what kind of DATA do we need in a NODE?
+            public Village nextVillage;
+            public string VillageName;
+            public bool isAstrildeHere = false;
+        }
+        
+
+
         }
     }
-}
+
+
